65ffae9432ddf58ddb0df2aec0d774182152f3df
Karl-Martin Skontorp <kms@skontorp.net>
2006-04-24 17:48:44 +0000

    git-svn-id: svn+ssh://ginandtonic.no/home/kms/.svnrepos/scout-kenwood@39 bec0a6a6-59ba-0310-824c-86e0bd1f4ed1

--- fRound.h
+++ fRound.h
@@ -6,25 +6,7 @@
 typedef struct {
     volatile uint32_t lower;
     volatile uint32_t step;
-} freq_band;
-
-typedef struct {
-    volatile freq_band *band;
-    volatile uint8_t size;
 } freq_bands;
-
-freq_bands bands = {{
-    {0, 1},
-    {10000000, 1},
-    {29400000, 10000},
-    {30000000, 12500},
-    {89000000, 100000},
-    {108000000, 25000},
-    {130000000, 12500},
-    {144000000, 1},
-    {144500000, 12500},
-    {145000000, 25000},
-}, 20};
 
 uint8_t findBand(uint32_t freq);
 uint32_t roundFreq(uint32_t freq);

f95ebc3b30b1406598bfca2237fe46ea17698db8
Karl-Martin Skontorp <kms@skontorp.net>
2006-05-20 11:03:43 +0000

    git-svn-id: svn+ssh://ginandtonic.no/home/kms/.svnrepos/scout-kenwood@86 bec0a6a6-59ba-0310-824c-86e0bd1f4ed1

--- fRound.c
+++ fRound.c
@@ -17,22 +17,22 @@ uint32_t steps[] = {12500};
 uint32_t roundFreq(uint32_t freq) {
     uint8_t band;
     uint32_t r;
-    
+
     for (band = 1; band < NUMBER_OF_BANDS; band++) {
 	if ((bands[band - 1].lower <= freq)
 		&& (bands[band].lower > freq)) {
 	    break;
 	}
     }
-    
+
     band--;
-    
+
     uint32_t step = steps[bands[band].step];
     r = freq % step;
 
     if (r >= (step / 2)) {
 	freq += step;
     }
-    
+
     return freq - r;
 }
